87eb0bbbffa07d321884f3cee1871d7486af7386
lindsayowow <lindsayow@gmail.com>
2026-05-27 19:05:33 -0500

    uploading base files to github

---
+++ src/main/java/org/example/Artifact.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Artifact {
+    public String name;
+    public int catalogId;
+    public String originDate;
+
+    public Artifact() {
+        this.name = "";
+        this.catalogId = 0;
+        this.originDate = "";
+    }
+}
---
+++ src/main/java/org/example/Possessed.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Possessed extends Artifact {
+    public String spiritName;
+
+    public Possessed() {
+        super();
+        this.spiritName = "";
+    }
+
+}
